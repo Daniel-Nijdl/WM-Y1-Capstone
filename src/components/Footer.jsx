b806e2f55bb5fc605a1f6ce3414ebffeb009e0cd
Ilya Tate <Itate265@west-mec.local>
2021-05-06 13:58:19 -0700

    Test

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'react-bootstrap';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      div.
+    </footer>
+  );
+}
+
+export default Footer;
