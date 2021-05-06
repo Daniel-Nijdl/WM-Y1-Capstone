d0c63beaf510740947d118e91d15d414db55a335
Ilya Tate <Itate265@west-mec.local>
2021-05-06 15:00:34 -0700

    Footer, nav, props.json

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import Navbar from './components/Navbar.jsx';
-
+import Navigation from './components/Navigation.jsx';
+import Footer from './components/Footer.jsx'
 
 function App() {
   return (
     <div>
-      <Navbar />
+      <Navigation />
+      <Footer />
   </div>
   );
 }
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,9 +1,10 @@
+import { Navigation as props } from '../props.json'
 import { Navbar, Nav } from 'react-bootstrap';
 
 const Navigation = () => {
   return(
-      <Navbar bg="dark" variant="dark">
-        <Navbar.Brand href="#top"><img src="#" alt="Logo" width="30" height="30" className="d-inline-block align-top" />Weather Works</Navbar.Brand>
+    <Navbar style={ { backgroundColor: props.backgroundColor } }>
+        <Navbar.Brand href="#top"><img src="../logo.png" alt="Logo" width="30" height="30" className="d-inline-block align-top" />Weather Works</Navbar.Brand>
         <Nav className="mr-auto">
           <Nav.Link href="#top">Home</Nav.Link>
           <Nav.Link href="#">Today</Nav.Link>
