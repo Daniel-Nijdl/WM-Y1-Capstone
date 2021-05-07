89a1c4de4bab5d90dd36f1ec897e47e22d09341c
Ilya Tate <Itate265@west-mec.local>
2021-05-07 13:30:57 -0700

    Footer at bottom and finished content

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,14 +3,13 @@ import { MDBCol, MDBContainer, MDBRow, MDBFooter } from 'mdbreact';
 
 const Footer = () => {
   return (
-    <MDBFooter style={{ backgroundColor: props.backgroundColor }} className="font-small pt-4 mt-4">
+    <MDBFooter style={{ backgroundColor: props.backgroundColor }} className="font-small pt-4 mt-4 fixed-bottom">
       <MDBContainer fluid className="text-center text-md-left">
         <MDBRow>
           <MDBCol md="6">
-            <h5 className="title">Footer Content</h5>
-            <p>
-              Here you can use rows and columns here to organize your footer
-              content.
+            <h5 className="title">About Us</h5>
+            <p style={{ fontSize: '12px' }}>
+              Reprehenderit veniam magna cupidatat veniam elit nostrud est. Aute magna minim adipisicing pariatur do irure quis ullamco. Nostrud deserunt mollit proident mollit irure deserunt esse voluptate mollit minim. Adipisicing adipisicing incididunt fugiat tempor cillum et in minim consectetur. Deserunt laborum ad est Lorem ex sunt minim aliqua quis veniam sint esse. In non labore nisi anim aliquip reprehenderit reprehenderit. Irure incididunt aliqua sit irure id ullamco excepteur deserunt veniam ea adipisicing. Proident ea cillum aliqua aute magna tempor veniam. Excepteur tempor est sint eu incididunt non irure cupidatat incididunt. Consectetur voluptate deserunt cillum sit dolore et laboris eiusmod mollit nisi.
             </p>
           </MDBCol>
           <MDBCol md="6">
@@ -31,7 +30,7 @@ const Footer = () => {
       </MDBContainer>
       <div className="footer-copyright text-center py-3">
         <MDBContainer fluid>
-          &copy; { new Date().getFullYear() } Copyright: Weather Works
+          &copy; { new Date().getFullYear() } Weather Works
         </MDBContainer>
       </div>
     </MDBFooter>
