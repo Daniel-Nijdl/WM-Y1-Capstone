73c65607c004d6608854db2fc7e108104b4489fd
Ilya <ilyatate@gmail.com>
2021-05-07 15:10:52 -0700

    Added export file

---
+++ src/export.js
@@ -0,0 +1,3 @@
+export { default as Navigation } from './components/Navigation.jsx';
+export { default as Footer } from './components/Footer.jsx';
+export { default as DayGraph } from './components/DayGraph.jsx';
