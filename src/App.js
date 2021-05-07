7d671615a4902e672869820c94e36b086c588d73
Ilya <ilyatate@gmail.com>
2021-05-07 15:11:31 -0700

    Created DayGraph Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import Navigation from './components/Navigation.jsx';
-import Footer from './components/Footer.jsx'
-import Temp from './components/Temp'
+import { Navigation, Footer, DayGraph } from './export.js';
 
 function App() {
   return (
