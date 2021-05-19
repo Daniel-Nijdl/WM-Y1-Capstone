0564f3824ad52a7ac4fcbb29a07c658f6c8ed01e
Jason E <jasonenciso6@gmail.com>
2021-05-19 13:30:05 -0700

    editing the cards

--- src/components/WindSpeed.jsx
+++ src/components/WindSpeed.jsx
@@ -4,7 +4,7 @@ import { GiPaperWindmill } from 'react-icons/gi';
 const WindSpeed = () => {
   return (
     <div className="" id="windCard">
-      <div className="">
+      <div className="mainCDiv">
         <p className="card-text cardText">
           <h2 className="resText">{ `${weather.current.wind_speed}` }</h2>
           <p className="resText">MPH</p>
